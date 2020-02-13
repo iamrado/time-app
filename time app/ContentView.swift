f6d3a4cc3d4e6973a329c57b3150c76b183a6105
Radoslav Blasko <radoslav.blasko@gmail.com>
2020-02-13 13:33:25 +0000

    Initial Commit

---
+++ time app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  time app
+//
+//  Created by Radoslav Blasko on 13/02/2020.
+//  Copyright © 2020 Radoslav Blasko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
